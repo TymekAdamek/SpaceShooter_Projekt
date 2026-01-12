65c350e92d8e1b270466b8a20f980e84b640bbbd
wiktorbogacki9 <wiktorbogacki9@gmail.com>
2026-01-12 18:38:37 +0100

    Klasa Enemy
    Logika, mechanika i spawn

---
+++ SpaceShooterC++/Enemy.cpp
@@ -0,0 +1,37 @@
+#include "Enemy.h"
+#include <cstdlib> 
+#include <iostream>
+
+Enemy::Enemy() {
+    fallSpeed = 3.0f;
+
+    
+    if (!texture.loadFromFile("assets/enemy.png")) {
+        std::cout << "Error: assets/enemy.png not found!" << std::endl;
+    }
+    sprite.setTexture(texture);
+
+
+    sf::FloatRect bounds = sprite.getLocalBounds();
+    sprite.setOrigin(bounds.width / 2.0f, bounds.height / 2.0f);
+
+    
+    float randomX = static_cast<float>(rand() % 750 + 25); 
+    sprite.setPosition(randomX, -50.0f); 
+}
+
+void Enemy::update() {
+    sprite.move(0.0f, fallSpeed); 
+}
+
+void Enemy::draw(sf::RenderWindow& window) {
+    window.draw(sprite);
+}
+
+sf::FloatRect Enemy::getBounds() {
+    return sprite.getGlobalBounds();
+}
+
+bool Enemy::isOffScreen() {
+    return (sprite.getPosition().y > 650.0f);
+}
