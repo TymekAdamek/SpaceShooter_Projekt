d4fbaa6f08866cba9288027431469932db912f6e
wiktorbogacki9 <wiktorbogacki9@gmail.com>
2026-01-12 18:35:13 +0100

    Klasa Player
    Dodanie Player

--- SpaceShooterC++/Player.h
+++ SpaceShooterC++/Player.h
@@ -1,1 +1,27 @@
-#pragma once
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <SFML/Graphics.hpp>
+#include "Bullet.h"
+
+class Player {
+private:
+    sf::Sprite sprite;
+    sf::Texture texture;
+    sf::Texture bulletTexture; 
+    float speed;
+
+public:
+    Player(); 
+
+    void update(); 
+    void draw(sf::RenderWindow& window);
+
+    
+    Bullet shoot();
+
+    sf::Vector2f getPosition();
+    sf::FloatRect getBounds();
+};
+
+#endif
