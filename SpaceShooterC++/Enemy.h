30156cafd99c358706f770e0037ed719eba9a25f
wiktorbogacki9 <wiktorbogacki9@gmail.com>
2026-01-12 18:36:36 +0100

    Klasa Enemy
    Losowy spawn, spadanie w dol i tekstura wroga

--- SpaceShooterC++/Enemy.h
+++ SpaceShooterC++/Enemy.h
@@ -1,1 +1,21 @@
-#pragma once
+#ifndef ENEMY_H
+#define ENEMY_H
+
+#include <SFML/Graphics.hpp>
+
+class Enemy {
+private:
+    sf::Sprite sprite;
+    sf::Texture texture;
+    float fallSpeed;
+
+public:
+    Enemy(); 
+
+    void update();
+    void draw(sf::RenderWindow& window);
+    sf::FloatRect getBounds();
+    bool isOffScreen();
+};
+
+#endif
