c7673c582c5b6c634c0ec11de8d4b2c6de774d0e
wiktorbogacki9 <wiktorbogacki9@gmail.com>
2026-01-13 15:04:09 +0100

    Klasa Bullet
    Implementacja bullet.h

--- SpaceShooterC++/Bullet.h
+++ SpaceShooterC++/Bullet.h
@@ -1,1 +1,22 @@
+#ifndef BULLET_H
+#define BULLET_H
+
+#include <SFML/Graphics.hpp>
+
+class Bullet {
+private:
+    sf::Sprite sprite;
+    float speed;
+
+public:
+    
+    Bullet(sf::Texture& tex, float startX, float startY);
+
+    void update();
+    void draw(sf::RenderWindow& window);
+    sf::FloatRect getBounds();
+    bool isOffScreen();
+};
+
+#endif#pragma once
 
