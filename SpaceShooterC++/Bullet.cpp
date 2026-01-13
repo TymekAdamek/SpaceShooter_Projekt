247b000ff4690882e48d1f5efdf9aad46674988b
wiktorbogacki9 <wiktorbogacki9@gmail.com>
2026-01-13 15:03:38 +0100

    Klasa Bullet
    Implementacja klasy bullet

--- SpaceShooterC++/Bullet.cpp
+++ SpaceShooterC++/Bullet.cpp
@@ -1,1 +1,26 @@
+#include "Bullet.h"
 
+Bullet::Bullet(sf::Texture& tex, float startX, float startY) {
+    sprite.setTexture(tex);
+    speed = 10.0f;
+
+    sf::FloatRect bounds = sprite.getLocalBounds();
+    sprite.setOrigin(bounds.width / 2.0f, bounds.height / 2.0f);
+    sprite.setPosition(startX, startY);
+}
+
+void Bullet::update() {
+    sprite.move(0.0f, -speed); 
+}
+
+void Bullet::draw(sf::RenderWindow& window) {
+    window.draw(sprite);
+}
+
+sf::FloatRect Bullet::getBounds() {
+    return sprite.getGlobalBounds();
+}
+
+bool Bullet::isOffScreen() {
+    return (sprite.getPosition().y < -50.0f);
+}
