ee70887f4e156ffb6fd2b31e58443bc291b6817c
milosz <bjycdxkiut7dc@gmail.com>
2026-01-16 02:34:19 +0100

    Menu.h

--- SpaceShooterC++/Menu.h
+++ SpaceShooterC++/Menu.h
@@ -1,1 +1,26 @@
 #pragma once
+#ifndef MENU_H
+#define MENU_H
+
+#include <SFML/Graphics.hpp>
+#include <fstream>
+#include <string>
+
+class Menu {
+public:
+    // Konstruktor przyjmujący wymiary okna do centrowania napisów
+    Menu(float width, float height);
+    
+    // Metoda rysująca napisy w oknie
+    void draw(sf::RenderWindow &window);
+
+private:
+    sf::Font font;
+    sf::Text playText;
+    sf::Text exitText;
+};
+
+// Funkcja zapisująca zaszyfrowany wynik
+void zapiszWynik(int punkty);
+
+#endif
