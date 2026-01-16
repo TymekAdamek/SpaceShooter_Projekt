32ead07172f2141adebf00995edbc38209473b5c
tymek <tymekadamek@gmail.com>
2026-01-16 11:31:11 +0100

    game.h

--- SpaceShooterC++/Game.h
+++ SpaceShooterC++/Game.h
@@ -1,1 +1,57 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <iostream>
+
+//dodaje pliki Wiktora i Milosza
+#include "Player.h"
+#include "Enemy.h"
+#include "Menu.h"
+#include "Bullet.h"
+
+class Game {
+private:
+    sf::RenderWindow* window;
+
+    // T£O (Ruszaj¹ce siê morze)
+    sf::Texture bgTexture;
+    sf::Sprite bgSprite1;
+    sf::Sprite bgSprite2;
+    float bgSpeed;
+
+    //OBIEKTy
+    Player* player;
+    std::vector<Enemy*> enemies;
+    std::vector<Bullet> bullets; // TU PRZECHOWUJEMY KULE
+    Menu* menu;
+
+    //LOGIKA
+    float spawnTimer;
+    float spawnTimerMax;
+
+    // Cooldown strzelania
+    float attackCooldown;
+    float attackCooldownMax;
+
+    int points;
+    bool isGameOver;
+    bool inMenu;
+
+    void initWindow();
+    void initTextures();
+    void initPlayer();
+    void initEnemies();
+    void initMenu();
+
+public:
+    Game();
+    virtual ~Game();
+
+    void run();
+    void update();
+    void render();
+    void updatePollEvents();
+    void updateGame();
+    void updateMenu();
+    void endGame(); // Funkcja koñcz¹ca grê
+};
